abee524795d2ec030c923406ce344b6bac49ddd8
chris <chris920216@aol.com>
2017-03-08 04:16:08 -0800

    index.php test

--- wp-content/themes/twentysixteentest/index.php
+++ wp-content/themes/twentysixteentest/index.php
@@ -5,17 +5,24 @@
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+* E.g., it puts together the home page when no home.php file exists.
+*
+* @link http://codex.wordpress.org/Template_Hierarchy
+*
+* @package WordPress
+* @subpackage Twenty_Sixteen
+* @since Twenty Sixteen 1.0
+*/
 
 get_header(); ?>
-
+<?php 
+$querystr = "SELECT $wpdb->person.name FROM $wpdb->person";
+$names = $wpdb->get_results($querystr,OBJECT);
+?>
+<?php foreach($names as $nms):?>
+<?php echo $nms>
+<?php endforeach;?>
+<?php endif;?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -26,8 +33,7 @@ get_header(); ?>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 			<?php endif; ?>
-
-			<?php
+	<?php
 			// Start the loop.
 			while ( have_posts() ) : the_post();
 
